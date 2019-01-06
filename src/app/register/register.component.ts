f6b13cc224907649c87146911277856bac10bd4c
priyanka arundekar <PriyankaArondekar>
2019-01-06 11:11:16 +0530

    Testccommit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cide-ad-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
